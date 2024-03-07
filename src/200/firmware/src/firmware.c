065968042a152dd81518f6280c6f87ada38fd8b8
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-03-07 01:24:07 +0100

    New firmware

--- src/200/firmware/src/firmware.c
+++ src/200/firmware/src/firmware.c
@@ -5,14 +5,15 @@ extern unsigned int hwswcd_avg(unsigned int value1, unsigned int value2);
 void main(void) {
 	volatile unsigned int value1, value2, avg;
 
-	value1 = 5;
-	value2 = 9;
+	value1 = 3;
+	value2 = 4;
 
     avg = hwswcd_avg(value1, value2);
 
 	print_str("AVG(0x");
 	print_hex(value1, 2);
 	print_str(", ");
+	print_str("AVG(0x");
 	print_hex(value2, 2);	
 	print_str(") = 0x");
 	print_hex(avg, 2);
