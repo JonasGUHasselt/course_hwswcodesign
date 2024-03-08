1ae1ae33eb2a23e16ba80e323832f012d50d575b
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-03-08 05:02:05 +0100

    print_dec

--- src/100/firmware/src/print.c
+++ src/100/firmware/src/print.c
@@ -21,11 +21,36 @@ void print_str(const char *p)
 
 void print_dec(unsigned int val)
 {
-	print_chr('n');
-	print_chr('o');
-	print_chr('p');
-	print_chr('e');
-	print_chr('\n');
+	//laatste cijfer
+	unsigned int number_of_digits = 0;
+	char number_to_print[200];
+
+	while (val > 0)
+	{
+		unsigned int number = val;
+		unsigned int divison_result = 0;
+
+		while (number >= 10)
+		{
+			divison_result++;
+			number -= 10;
+		}
+
+		if (number_of_digits <= sizeof(number_to_print))
+		{
+			number_to_print[number_of_digits] = number;		
+		}
+
+		number_of_digits++;
+		
+		val = divison_result;
+	}
+
+	for (int i = number_of_digits-1; i >= 0; i--)
+	{
+		char number = 48 + number_to_print[i];
+		print_chr(number);
+	}
 }
 
 
