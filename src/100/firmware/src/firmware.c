84b187d335c7d05a2d2a55c5417f150fd2da5489
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-03-08 03:58:24 +0100

    reset excersises week1

--- src/100/firmware/src/firmware.c
+++ src/100/firmware/src/firmware.c
@@ -1,17 +1,9 @@
 #include "print.h"
 
-unsigned int convert(unsigned int x);
-
 void main(void) {
     print_chr('*');
 	print_str("\nhello world\n");
-    //print_dec(5);
     print_dec(64);
     print_dec(600);
     print_hex(64, 8);
-    print_dec(convert(86));
-}
-//13 clock cycles
-unsigned int convert(unsigned int x) {
-    return (unsigned int)((x - 32) * (5.0/9.0));
 }
