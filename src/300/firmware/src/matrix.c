31767e55edb182dc88422cab5b29251e761c989e
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-03-24 13:24:12 +0100

    new firmware

--- src/300/firmware/src/matrix.c
+++ src/300/firmware/src/matrix.c
@@ -1,14 +1,12 @@
 #include <stdint.h>
 #include "matrix.h"
-#include "counter.h"
 
 uint32_t multiply(uint32_t matrix_1, uint32_t matrix_2)
 {
     MATRIX_A = matrix_1;
     MATRIX_B = matrix_2;
     CE = 1;
-    counter_start();
-    counter_stop();
+    while(RESULT_READY != 1){}
     CE = 0;
     
     return RESULT_MATRIX;
--- src/300/firmware/src/matrix.h
+++ src/300/firmware/src/matrix.h
@@ -10,11 +10,13 @@
 #define SINE_REG1_ADDRESS     (SINE_BASEADDRESS + 1*4)
 #define SINE_REG2_ADDRESS     (SINE_BASEADDRESS + 2*4)
 #define SINE_REG3_ADDRESS     (SINE_BASEADDRESS + 3*4)
+#define SINE_REG4_ADDRESS     (SINE_BASEADDRESS + 4*4)
       
 #define CE              (*(volatile unsigned int *) SINE_REG0_ADDRESS)
 #define MATRIX_A        (*(volatile unsigned int *) SINE_REG1_ADDRESS)
 #define MATRIX_B        (*(volatile unsigned int *) SINE_REG2_ADDRESS)
 #define RESULT_MATRIX   (*(volatile unsigned int *) SINE_REG3_ADDRESS)
+#define RESULT_READY    (*(volatile unsigned int *) SINE_REG4_ADDRESS)
 
 uint32_t multiply(uint32_t matrix_1, uint32_t matrix_2);
 
