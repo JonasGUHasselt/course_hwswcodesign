a956672efb7f901600c8faf5fea2ff315a837767
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-03-24 10:03:11 +0100

    new firmware

--- src/300/firmware/src/matrix.c
+++ src/300/firmware/src/matrix.c
@@ -1,11 +1,15 @@
 #include <stdint.h>
 #include "matrix.h"
+#include "counter.h"
 
 uint32_t multiply(uint32_t matrix_1, uint32_t matrix_2)
 {
     MATRIX_A = matrix_1;
     MATRIX_B = matrix_2;
     CE = 1;
+    counter.start();
+    counter.stop();
+    CE = 0;
     
     return RESULT_MATRIX;
 }
