0299f51ebc43b542836d33461eab3b815bbb2c43
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-03-24 17:41:55 +0100

    software fix

--- src/300/firmware/src/matrix.c
+++ src/300/firmware/src/matrix.c
@@ -3,17 +3,18 @@
 
 uint32_t multiply(uint32_t matrix_1, uint32_t matrix_2)
 {
+    uint32_t temp;
     uint32_t result = ((matrix_1 & 0xff000000) >> 24) * ((matrix_2 & 0xff000000) >> 24);
     result += ((matrix_1 & 0x00ff0000) >> 16) * ((matrix_2 & 0x0000ff00) >> 8);
     result <<= 24;
 
-    result += ((matrix_1 & 0xff000000) >> 24) * ((matrix_2 & 0x00ff0000) >> 16);
-    result += ((matrix_1 & 0x00ff0000) >> 16) * ((matrix_2 & 0x000000ff));
-    result <<=16;
+    temp = ((matrix_1 & 0xff000000) >> 24) * ((matrix_2 & 0x00ff0000) >> 16);
+    temp += ((matrix_1 & 0x00ff0000) >> 16) * ((matrix_2 & 0x000000ff));
+    result += temp << 16;
 
-    result += ((matrix_1 & 0x00ff0000) >> 16) * ((matrix_2 & 0xff000000) >> 24);
-    result += ((matrix_1 & 0x000000ff)) * ((matrix_2 & 0x0000ff00) >> 8);
-    result <<=8;
+    temp = ((matrix_1 & 0x00ff0000) >> 16) * ((matrix_2 & 0xff000000) >> 24);
+    temp += ((matrix_1 & 0x000000ff)) * ((matrix_2 & 0x0000ff00) >> 8);
+    result += temp << 8;
 
     result += ((matrix_1 & 0x0000ff00) >> 8) * ((matrix_2 & 0x00ff0000) >> 16);
     result += ((matrix_1 & 0x000000ff)) * ((matrix_2 & 0x000000ff));
