d30393f45733e9b4076ef644e751e0cba72e7f3e
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-03-24 13:44:04 +0100

    new firmware

--- src/300/firmware/src/matrix.c
+++ src/300/firmware/src/matrix.c
@@ -6,8 +6,8 @@ uint32_t multiply(uint32_t matrix_1, uint32_t matrix_2)
     MATRIX_A = matrix_1;
     MATRIX_B = matrix_2;
     CE = 1;
-    while(RESULT_READY != 1){}
     CE = 0;
+    while(RESULT_READY != 1){}
     
     return RESULT_MATRIX;
 }
