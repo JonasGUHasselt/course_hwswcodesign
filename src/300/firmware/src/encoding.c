fd1182b69ae98a5603af6102eca057bb9d98830a
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-04-29 01:52:10 +0200

    debug

--- src/300/firmware/src/encoding.c
+++ src/300/firmware/src/encoding.c
@@ -45,8 +45,6 @@ void construct_body(void)
         // unsigned int pixel = *(IMAGE_PIXEL_VALUE + i);
         print_chr('\n');
         print_chr('\n');
-        print_chr('k');
-        print_hex(*((volatile unsigned int *) IMAGE_REG2_ADDRESS + i - 4), 8);
         print_chr('p');
         print_hex(pixel, 8);
         print_chr('l');
