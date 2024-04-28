e0e7a453a825f2d0dce1117448fc80102e4dad2c
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-04-29 00:54:55 +0200

    fix bug

--- src/300/firmware/src/encoding.c
+++ src/300/firmware/src/encoding.c
@@ -38,18 +38,18 @@ void construct_body(void)
     int number_of_pixels = IMAGE_WIDTH * IMAGE_HEIGHT;
     print_chr('\n');
     print_dec(IMAGE_WIDTH);
-    print_chr('\n');
-    print_dec(IMAGE_HEIGHT);
-    print_chr('\n');
-    print_dec(number_of_pixels);
-    print_chr('\n');
+    // print_chr('\n');
+    // print_dec(IMAGE_HEIGHT);
+    // print_chr('\n');
+    // print_dec(number_of_pixels);
+    // print_chr('\n');
     for (int i=0; i < number_of_pixels*4 ; i+=4)
     {
         pixel_handled = false;
         unsigned int pixel = IMAGE_PIXEL_VALUE + i;
-        print_chr('\n');
-        print_hex(pixel, 16);
-        print_chr('\n');
+        // print_chr('\n');
+        // print_hex(pixel, 16);
+        // print_chr('\n');
         
         if (is_previous_pixel(pixel))
         {
