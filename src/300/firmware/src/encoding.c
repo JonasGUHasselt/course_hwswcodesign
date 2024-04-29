4ff72980780968222f695301dd1d78bc2f28ac3a
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-04-29 19:27:11 +0200

    debug

--- src/300/firmware/src/encoding.c
+++ src/300/firmware/src/encoding.c
@@ -200,9 +200,9 @@ bool can_calculate_difference(unsigned int pixel)
         print_chr('\n');
         print_chr('\n');
         if (value != (char)0xFF && value !=(char)0x00){return false;}
-        
-        return true;
-    }
+    }   
+    
+    return true;
 }
 
 unsigned int calculate_difference(unsigned int pixel)
