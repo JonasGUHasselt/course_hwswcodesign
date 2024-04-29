4add7446576f861e77f264944c51ea6cfc77c8c8
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-04-29 06:49:23 +0200

    debug

--- src/300/firmware/src/encoding.c
+++ src/300/firmware/src/encoding.c
@@ -221,29 +221,14 @@ unsigned int calculate_difference(unsigned int pixel)
 
 void save_difference_encoding(unsigned int difference)
 {
-    print_chr('\n');
-    print_hex(difference, 8);
     unsigned char difference_chunk = 0b01 << 2;
-    print_chr('\n');
-    print_chr('\n');
-    print_hex(((signed char)((difference >> 16) & 0xFF)) + 2, 8);
-    print_chr('\n');
-    print_hex((((difference >> 8) & 0xFF)) + 2, 8);
-    print_chr('\n');
-    print_hex(((signed char) (difference & 0xFF)) + 2, 8);
-    print_chr('\n');
-    print_chr('\n');
     
     difference_chunk += ((signed char)((difference >> 16) & 0xFF)) + 2;
     difference_chunk <<= 2;
-    print_hex(difference_chunk, 2);
     difference_chunk += ((signed char)((difference >> 8) & 0xFF)) + 2;
     difference_chunk <<= 2;
-    print_chr('\n');
-    print_hex(difference_chunk, 2);
     difference_chunk += ((signed char) (difference & 0xFF)) + 2;
-    print_chr('\n');
-    print_hex(difference_chunk, 2);
+
     
     print_hex(difference_chunk, 2);
 }
