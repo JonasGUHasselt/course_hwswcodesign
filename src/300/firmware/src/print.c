b66441612412238221e2db47646a6ac2332a7247
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-04-29 03:41:58 +0200

    debug

--- src/300/firmware/src/print.c
+++ src/300/firmware/src/print.c
@@ -21,13 +21,12 @@ void print_str(const char *p)
 
 void print_dec(unsigned int val)
 {
-	char buffer[10];
+	char buffer[100];
 	char *p = buffer;
 	while (val || p == buffer) {
 		print_chr('c');
 
-		*p = (unsigned char)(val % 10);
-		p = p + 1;
+		*(p++) = (unsigned char)(val % 10);
 		print_chr('l');
 		val = val / 10;
 		print_chr('a');
