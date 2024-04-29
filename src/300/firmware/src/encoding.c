1f86ecbe6a95b2f392c688003d916c5e46317ce7
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-04-29 08:39:17 +0200

    debug

--- src/300/firmware/src/encoding.c
+++ src/300/firmware/src/encoding.c
@@ -212,26 +212,15 @@ unsigned int calculate_difference(unsigned int pixel)
     unsigned char blue_value = current_pixel[2];
 
     difference += red_value - previous_pixel[0];
+    print_chr('\n');
+    print_chr('\n');
+    print_hex(difference, 8);
+    print_chr('\n');
+    print_chr('\n');
     difference <<= 8;
     difference += green_value - previous_pixel[1];
     difference <<= 8;
     difference += blue_value - previous_pixel[2];
-    print_chr('\n');
-    print_hex(red_value, 2);
-    print_chr('\n');
-    print_hex(green_value, 2);
-    print_chr('\n');
-    print_hex(blue_value, 2);
-    print_chr('\n');
-    print_hex(previous_pixel[0], 2);
-    print_chr('\n');
-    print_hex(previous_pixel[1], 2);
-    print_chr('\n');
-    print_hex(previous_pixel[2], 2);
-    print_chr('\n');
-    print_hex(previous_pixel[3], 2);
-
-    print_chr('\n');
     
     return difference;
 }
