8f2cd636415794f7d6e32224b7504aa8034959d2
Jonas Ghori <jonas.ghori@student.uhasselt.be>
2024-04-29 03:17:43 +0200

    debug

--- src/300/firmware/src/print.c
+++ src/300/firmware/src/print.c
@@ -24,13 +24,12 @@ void print_dec(unsigned int val)
 	char buffer[10];
 	char *p = buffer;
 	// while (val || p == buffer) {
-	while (val != 0) {
-		print_chr('c');
-		*(p++) = (unsigned char )(val % 10);
-		print_chr('l');
-		val = val / 10;
-		print_chr('a');
-	}
+	// 	print_chr('c');
+	// 	*(p++) = (unsigned char )(val % 10);
+	// 	print_chr('l');
+	// 	val = val / 10;
+	// 	print_chr('a');
+	// }
 	print_chr('i');
 	while (p != buffer) {
 		print_chr('n');
